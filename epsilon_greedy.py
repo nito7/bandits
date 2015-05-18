128830b9d616ca46a68745a93c79b0a69c68a06c
nito7 <naoki.itoh.7kusa@gmail.com>
2015-05-18 19:09:02 +0900

    add library

---
+++ epsilon_greedy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+def process():
+    execfile("core.py")
+    arm_rates = [0.012, 0.013, 0.013, 0.012, 0.012, 0.012, 0.011, 0.011, 0.011, 0.01, 0.01]
+    arms = [BernoulliArm(r) for r in arm_rates]
+    counts = [10000, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    values = [0.012, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    algo1 = EpsilonGreedy(0.1, counts, values)
+    for t in range(1000):
+        chosen_arm = algo1.select_arm()
+        reward = arms[chosen_arm].draw()
+        algo1.update(chosen_arm, reward)
+
+if __name__ == "__main__":
+    process()
